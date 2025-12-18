46c69254f1281a58e341cc585b95b5226772f5b8
apurvag1 <apurvag.130@gmail.com>
2025-12-18 23:25:31 +0530

    [Apurva Gupta] Created and added average percent marks

---
+++ gcr-codebase/programming-elements/averageMarks.java
@@ -0,0 +1,9 @@
+public class averageMarks {
+    public static void main(String[] args) {
+        double maths=94;
+        double physics=95;
+        double chemistry=93;
+        double averageMarks= (maths + physics + chemistry) / 3.0;
+        System.out.println("Average marks of the student: " + averageMarks);
+    }
+}
