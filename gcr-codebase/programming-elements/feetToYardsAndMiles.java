cac53749c12919f24f04116ec80e1d355a6b18e0
apurvag1 <apurvag.130@gmail.com>
2025-12-18 23:41:08 +0530

    [Apurva Gupta] Created and added Convert distance in yards and miles

---
+++ gcr-codebase/programming-elements/feetToYardsAndMiles.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class feetToYardsAndMiles {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the distance in feet: ");   
+        double feet = scan.nextDouble();                       //Takes distance input in feet
+        double yards = feet / 3;                               //Converts feet to yards
+        double miles = yards / 1760;                            //Converts feet to miles
+        System.out.println("The distance of " + feet + " feet is equal to " + yards + " yards or " + miles + " miles.");
+        scan.close();
+    }
+}
