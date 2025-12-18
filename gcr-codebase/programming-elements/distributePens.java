e42c2a13c2db1fabac9ee9de5bf25559ddafa630
apurvag1 <apurvag.130@gmail.com>
2025-12-18 23:28:31 +0530

    [Apurva Gupta] Created and added distribute pens

---
+++ gcr-codebase/programming-elements/distributePens.java
@@ -0,0 +1,9 @@
+public class distributePens {
+    public static void main(String[] args) {
+        int pens=14;
+        int students=3;
+        int pensPerStudent= pens / students;
+        int remainingPens= pens % students; 
+        System.out.println("The pens per student are: " + pensPerStudent+" and the remaining pens not distributed are: " + remainingPens);
+    }
+}
