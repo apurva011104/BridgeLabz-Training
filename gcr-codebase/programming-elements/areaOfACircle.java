b598ffe628558ff1e138e80889faa7974330de52
apurvag1 <apurvag.130@gmail.com>
2025-12-18 23:19:48 +0530

    [Apurva Gupta] Created and added area of a circle

---
+++ gcr-codebase/programming-elements/areaOfACircle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class areaOfACircle {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter radius of the circle: ");   
+        double radius = scan.nextDouble();                   //Takes radius input
+        double area = Math.PI * radius * radius;            //Calculates area of the circle
+        System.out.println("Area of the circle: "+area);
+        scan.close();
+    }
+}
