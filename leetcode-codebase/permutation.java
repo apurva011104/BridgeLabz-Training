85aceac9b2c7a557de0b22a674d211ad00a3c522
apurvag1 <apurvag.130@gmail.com>
2025-12-18 23:06:45 +0530

    [Apurva Gupta] Created and added permutation

---
+++ leetcode-codebase/permutation.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class permutation {
+    public void recurse(List<List<Integer>> ans, List<Integer> list, int []nums){
+        if(list.size()==nums.length){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(list.contains(nums[i]))      continue;
+            list.add(nums[i]);
+            recurse(ans,list,nums);
+            list.remove(list.size()-1);
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> answer=new ArrayList<>();
+        recurse(answer,new ArrayList<>(),nums);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        permutation p=new permutation();
+        int nums[]={1,2,3};
+        List<List<Integer>> result=p.permute(nums);
+        System.out.println("All permutations: ");
+        for(List<Integer> perm : result){
+            System.out.print(perm + " ");
+        }
+    }
+}
