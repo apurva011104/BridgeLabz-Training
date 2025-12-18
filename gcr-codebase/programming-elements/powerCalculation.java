c644490a52df830b59dc0e6f9c01daf123e2127f
apurvag1 <apurvag.130@gmail.com>
2025-12-18 23:18:33 +0530

    [Apurva Gupta] Created and added power calculation

---
+++ gcr-codebase/programming-elements/powerCalculation.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class powerCalculation {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter base number: ");        
+        int base = scan.nextInt();                     //Takes base number input
+        System.out.print("Enter exponent number: ");         
+        int exponent = scan.nextInt();                 //Takes exponent number input
+        double result = Math.pow(base, exponent);      //Calculates power
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+        scan.close();
+    }
+}
