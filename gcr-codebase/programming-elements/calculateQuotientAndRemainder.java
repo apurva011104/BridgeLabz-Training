4b22eadee3dfb40ee18bdad5ff3ac439e4758089
apurvag1 <apurvag.130@gmail.com>
2025-12-18 23:46:05 +0530

    [Apurva Gupta] Created and added calculate quotient and remainder

---
+++ gcr-codebase/programming-elements/calculateQuotientAndRemainder.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class calculateQuotientAndRemainder {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the dividend: ");   
+        int dividend = scan.nextInt();
+        System.out.print("Enter the divisor: ");
+        int divisor = scan.nextInt();
+        int quotient = dividend / divisor;                     //Calculates quotient
+        int remainder = dividend % divisor;                    //Calculates remainder   
+        System.out.println("The quotient is: " + quotient + " and the remainder is: " + remainder + " when " + dividend + " is divided by " + divisor + ".");
+        scan.close();   
+    }
+}
