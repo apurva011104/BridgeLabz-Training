daf65f4bf3206f33c068740def802c48fce82dfc
apurvag1 <apurvag.130@gmail.com>
2025-12-18 23:38:22 +0530

    [Apurva Gupta] Created and added area of a triangle

---
+++ gcr-codebase/programming-elements/areaOfATriangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class areaOfATriangle {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the base of the triangle (in cms): ");   
+        double base = scan.nextDouble();                       //Takes base input
+        System.out.print("Enter the height of the triangle (in cms): ");
+        double height = scan.nextDouble();                     //Takes height input 
+        double areaInCms = 0.5 * base * height;                     //Calculates area of the triangle
+        double areaInInches = areaInCms / (2.54 * 2.54);                             //Converts area from cms to inches
+        System.out.println("The area of the triangle in square centimeters is "+ areaInCms +"while in square inches is" + areaInInches + ".");
+        scan.close();
+    }
+    
+}
