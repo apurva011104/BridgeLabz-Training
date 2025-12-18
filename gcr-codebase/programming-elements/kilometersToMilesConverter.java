933d53e6cb87cc31d2b03f1528885e302aa57d71
apurvag1 <apurvag.130@gmail.com>
2025-12-18 23:26:45 +0530

    [Apurva Gupta] Created and added kilometers to miles conversion

---
+++ gcr-codebase/programming-elements/kilometersToMilesConverter.java
@@ -0,0 +1,7 @@
+public class kilometersToMilesConverter {
+    public static void main(String[] args) {
+        double kilometers = 10.8;                          //Example distance in kilometers
+        double miles = kilometers * 0.6;              //Converts kilometers to miles
+        System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
+    }
+}
