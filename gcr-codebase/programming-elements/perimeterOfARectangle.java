8d48313d2c56f135932a494aab779dec70dee63a
apurvag1 <apurvag.130@gmail.com>
2025-12-18 23:13:20 +0530

    [Apurva Gupta] Created and added perimeter of a rectangle

---
+++ gcr-codebase/programming-elements/perimeterOfARectangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class perimeterOfARectangle {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter length of the rectangle: ");
+        double length = scan.nextDouble();                 //Takes length input
+        System.out.print("Enter breadth of the rectangle: ");
+        double breadth = scan.nextDouble();                //Takes breadth input
+        double perimeter = 2 * (length + breadth);         //Calculates perimeter of the rectangle
+        System.out.println("Perimeter of the rectangle: " + perimeter);
+    }
+}
