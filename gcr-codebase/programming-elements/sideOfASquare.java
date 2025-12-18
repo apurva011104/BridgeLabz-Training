a3ee49723f8ef7a6b9d8172e542dca9e8cd28c70
apurvag1 <apurvag.130@gmail.com>
2025-12-18 23:39:29 +0530

    [Apurva Gupta] Created and added side of a square

---
+++ gcr-codebase/programming-elements/sideOfASquare.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class sideOfASquare {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the perimeter of the square (in cms): ");
+        double perimeter = scan.nextDouble();
+        double side = perimeter / 4;
+        System.out.println("The side of the square is: " + side + " cms whose perimeter is " + perimeter + " cms.");
+        scan.close();
+    }
+    
+}
