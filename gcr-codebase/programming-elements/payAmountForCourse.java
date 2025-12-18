0009df240c0505bdc502a9f40acec30a73c10d49
apurvag1 <apurvag.130@gmail.com>
2025-12-18 23:29:42 +0530

    [Apurva Gupta] Created and added pay amount for a course

---
+++ gcr-codebase/programming-elements/payAmountForCourse.java
@@ -0,0 +1,9 @@
+public class payAmountForCourse {
+    public static void main(String[] args) {
+        double fee=125000.0;
+        double discountPercentage=10.0;
+        double discount= (discountPercentage * fee) / 100.0;
+        double amountToPay= fee - discount;
+        System.out.println("The discount amount is " + discount + " and the final discounted fee is INR " + amountToPay);
+    }
+}
