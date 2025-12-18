a3daec80a4b546e50dbac03fc380455a244f2ef8
apurvag1 <apurvag.130@gmail.com>
2025-12-18 23:42:11 +0530

    [Apurva Gupta] Created and added price calculator

---
+++ gcr-codebase/programming-elements/calculatePrice.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class calculatePrice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the price of the item: ");
+        double price = scan.nextDouble();
+        System.out.println("Enter quantity of the item: ");
+        int quantity = scan.nextInt();
+        double totalPrice = price * quantity;
+        System.out.println("The total purchase price is INR " + totalPrice + " if the quantity purchased is " + quantity + " and the price per item is INR " + price + ".");
+        scan.close();
+    }
+}
