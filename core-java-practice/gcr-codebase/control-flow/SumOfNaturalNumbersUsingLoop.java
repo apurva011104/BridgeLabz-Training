2d8b4a90b4a49f01913af68744054705af53ead0
apurva011104 <akaul7817@gmail.com>
2025-12-19 16:12:49 +0530

    [Apurva Gupta] Add .Added sum of natural numbers using loop

---
+++ core-java-practice/gcr-codebase/control-flow/SumOfNaturalNumbersUsingLoop.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNumbersUsingLoop {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = scan.nextInt();
+        if(num <1){
+            System.out.println("Not a natural number.");
+        } 
+        else{
+            int sum = 0;
+            for(int i=1; i<=num; i++){
+                sum += i;
+            }
+            int sumUsingFormula = (num * (num + 1)) / 2;
+            boolean isCorrect = (sum == sumUsingFormula);
+            if(isCorrect){
+                System.out.printf("The sum of %d natural numbers using formula %d and using loop %d are equal.",num,sumUsingFormula,sum);
+            }
+            else{
+                System.out.printf("The sum of %d natural numbers using formula %d and using loop %d are not equal.",num,sumUsingFormula,sum);
+            }
+        }
+        scan.close();
+    }
+}
