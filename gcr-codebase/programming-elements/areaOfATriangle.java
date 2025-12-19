c7d623ec16352cf5a73d896b1a8aabe0fd7c9aed
apurva011104 <akaul7817@gmail.com>
2025-12-19 10:51:35 +0530

    [Apurva Gupta] Refactored area of a triangle

--- gcr-codebase/programming-elements/areaOfATriangle.java
+++ gcr-codebase/programming-elements/areaOfATriangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class areaOfATriangle {
+public class AreaOfATriangle {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the base of the triangle (in cms): ");   
