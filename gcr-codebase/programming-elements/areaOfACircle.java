f6fa2a2943b2d57d961de6719518f93a078e066d
apurva011104 <akaul7817@gmail.com>
2025-12-19 10:50:33 +0530

    [Apurva Gupta] Refactored area of a circle

--- gcr-codebase/programming-elements/areaOfACircle.java
+++ gcr-codebase/programming-elements/areaOfACircle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class areaOfACircle {
+public class AreaOfACircle {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter radius of the circle: ");   
