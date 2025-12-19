b0ee2b5b4cc1a004bfaa4d5ace1cb70a5463a694
apurva011104 <akaul7817@gmail.com>
2025-12-19 11:07:14 +0530

    [Apurva Gupta] Refactored Permutation

--- core-java-practice/leetcode-codebase/Permutation.java
+++ core-java-practice/leetcode-codebase/Permutation.java
@@ -3,15 +3,15 @@ import java.util.List;
 
 public class Permutation {
     public void recurse(List<List<Integer>> ans, List<Integer> list, int []nums){
-        if(list.size()==nums.length){
+        if(list.size()==nums.length){               //Base case: if the current list size equals the input array length, we found a permutation
             ans.add(new ArrayList<>(list));
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(list.contains(nums[i]))      continue;
-            list.add(nums[i]);
+        for(int i=0;i<nums.length;i++){     //Iterate through the input array
+            if(list.contains(nums[i]))      continue;               //Skip if the number is already in the current permutation
+            list.add(nums[i]);          //Add the number to the current permutation
             recurse(ans,list,nums);
-            list.remove(list.size()-1);
+            list.remove(list.size()-1);    //Backtrack: remove the last added number to explore other permutations
         }
     }
     public List<List<Integer>> permute(int[] nums) {
