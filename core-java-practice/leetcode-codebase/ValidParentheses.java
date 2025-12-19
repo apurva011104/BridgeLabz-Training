caa394cbb8a721b7be48f5b6d924a212f25f182f
apurva011104 <akaul7817@gmail.com>
2025-12-19 11:04:57 +0530

    [Apurva Gupta] Refactored Valid Parentheses

--- core-java-practice/leetcode-codebase/ValidParentheses.java
+++ core-java-practice/leetcode-codebase/ValidParentheses.java
@@ -1,12 +1,13 @@
 import java.util.Stack;
 
 public class ValidParentheses {
+
     public boolean isValid(String s) {
-        Stack<Character> check=new Stack<>();
+        Stack<Character> check=new Stack<>();       //Initialize a stack to keep track of opening parentheses
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='[')
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='[')       //If it's an opening parentheses, push it onto the stack 
                 check.push(s.charAt(i));
-            else{
+            else{                                                      //If it's a closing parentheses, check for matching opening parentheses
                 if(check.isEmpty())
                     return false;
                 else if(s.charAt(i)==')' && check.peek()!='(')
