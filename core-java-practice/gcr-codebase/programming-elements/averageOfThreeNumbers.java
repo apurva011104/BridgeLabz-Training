f61ec0886bede249e7d3a2fb9b6ae17a90535901
apurva011104 <akaul7817@gmail.com>
2025-12-19 11:24:20 +0530

    [Apurva Gupta] Refactored average of three numbers

--- core-java-practice/gcr-codebase/programming-elements/averageOfThreeNumbers.java
+++ core-java-practice/gcr-codebase/programming-elements/averageOfThreeNumbers.java
@@ -1,4 +1,4 @@
-public class averageOfThreeNumbers {
+public class AverageOfThreeNumbers {
     public static void main(String[] args) {
         java.util.Scanner scan = new java.util.Scanner(System.in);
         System.out.print("Enter first number: ");
