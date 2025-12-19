e79f5a4d7018dbbaaf1978b86c6fee4bff87e581
apurva011104 <akaul7817@gmail.com>
2025-12-19 16:04:12 +0530

    [Apurva Gupta] Refactor .Fixed calculate the sum of numbers until users enters zero

--- core-java-practice/gcr-codebase/control-flow/CalculateSumUntilZero.java
+++ core-java-practice/gcr-codebase/control-flow/CalculateSumUntilZero.java
@@ -5,13 +5,12 @@ public class CalculateSumUntilZero {
         Scanner scan = new Scanner(System.in);
         double sum = 0.0;
         System.out.println("Enter numbers to add and enter 0 to print their sum.");
-        while(true){
-            System.out.print("Enter a number: ");
-            double value = scan.nextDouble();
-            if(value == 0.0){
-                break;
-            }
+        System.out.print("Enter a number: ");
+        double value = scan.nextDouble();
+        while(value == 0.0){
             sum += value;
+            System.out.print("Enter a number: ");
+            value = scan.nextDouble();
         }
         System.out.printf("The sum is: %.6f", sum);
         scan.close();
