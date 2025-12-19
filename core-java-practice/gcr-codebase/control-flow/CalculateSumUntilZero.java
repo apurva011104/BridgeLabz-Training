6828bde42a4ea8e89f70113f7d702ff997d88e84
apurva011104 <akaul7817@gmail.com>
2025-12-19 16:02:06 +0530

    [Apurva Gupta] Add .Added calculate the sum of numbers until users enters zero

---
+++ core-java-practice/gcr-codebase/control-flow/CalculateSumUntilZero.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CalculateSumUntilZero {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double sum = 0.0;
+        System.out.println("Enter numbers to add and enter 0 to print their sum.");
+        while(true){
+            System.out.print("Enter a number: ");
+            double value = scan.nextDouble();
+            if(value == 0.0){
+                break;
+            }
+            sum += value;
+        }
+        System.out.printf("The sum is: %.6f", sum);
+        scan.close();
+    }
+}
