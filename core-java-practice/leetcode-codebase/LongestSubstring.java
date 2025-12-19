81b8112ceb41684f716aac8d285db65ba6adbdd8
apurva011104 <akaul7817@gmail.com>
2025-12-19 11:13:54 +0530

    [Apurva Gupta] Refactored Longest Substring without repeating Characters

--- core-java-practice/leetcode-codebase/LongestSubstring.java
+++ core-java-practice/leetcode-codebase/LongestSubstring.java
@@ -1,33 +1,36 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class LongestSubstring {
+
     public static boolean isUnique(String s){
-        char []ch=new char[s.length()];
-        for(int i=0;i<s.length();i++)
-            ch[i]=s.charAt(i);
-        Arrays.sort(ch);
-        for(int i=1;i<s.length();i++){
-            if(ch[i-1]==ch[i])
-            return false;
+        Set<Character> set=new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            if(set.contains(s.charAt(i))){      //If the character is already in the set, it's not unique
+                return false;
+            }
+            set.add(s.charAt(i));        //Add the character to the set
         }
         return true;
     }
+
     public int lengthOfLongestSubstring(String s) {
-        if(s.length()==0)   return 0;
-        List<Integer> len=new  ArrayList<>();
+        if(s.length()==0){      //Edge case: if the string is empty, return 0
+            return 0;
+        }
+        int maxLen=0;
         for(int i=0;i<s.length();i++){
             for(int j=i+1;j<=s.length();j++){
-                if(isUnique(s.substring(i,j)))
-                len.add(j-i);
-                else
-                break;
+                if(isUnique(s.substring(i,j))){      //Check if the substring has all unique characters
+                    maxLen=Math.max(maxLen,j-i);        //Update maxLen if the current substring is longer
+                }
+                else{
+                    break;
+                }
             }
         }
-        Collections.sort(len);
-        return len.get(len.size()-1);
+        return maxLen;
     }
     public static void main(String[] args) {
         LongestSubstring ls=new LongestSubstring();
