2bc71dea186b6f608028121fdd0229e1b90cbb84
apurva011104 <akaul7817@gmail.com>
2025-12-19 16:58:27 +0530

    [Apurva Gupta] Add .Added factors of a number

---
+++ core-java-practice/gcr-codebase/control-flow/FactorsOfANumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FactorsOfANumber {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num=scan.nextInt();
+        if(num<=0){
+            System.out.println("Not a positive integer.");
+            return;
+        }
+        System.out.print("Factors of "+num+" are:");
+        for(int i=1;i<=num;i++){
+            if(num%i==0){
+                System.out.print(" "+i);
+            }
+        }
+    }
+}
