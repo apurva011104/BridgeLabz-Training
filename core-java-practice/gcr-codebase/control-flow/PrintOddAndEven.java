928940196abfb587c6cbbee1ec52f0864b1f7355
apurva011104 <akaul7817@gmail.com>
2025-12-19 16:39:59 +0530

    [Apurva Gupta] Refactor .Changed the logic

--- core-java-practice/gcr-codebase/control-flow/PrintOddAndEven.java
+++ core-java-practice/gcr-codebase/control-flow/PrintOddAndEven.java
@@ -8,18 +8,9 @@ public class PrintOddAndEven {
             System.out.println("Not a positive integer.");
             return;
         }
-        System.out.print("Even numbers: ");
         for(int i=1;i<=num;i++){
-            if(i%2==0){
-                System.out.print(i+" ");
-            }
-        }
-        System.out.println();
-        System.out.print("Odd numbers: ");
-        for(int i=1;i<=num;i++){
-            if(i%2==1){
-                System.out.print(i+" ");
-            }
+            System.out.println( i+" is "+( i%2==0 ? "Even" : "Odd" ) );
+
         }
     }
 }
