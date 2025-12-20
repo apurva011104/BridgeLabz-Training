bfb9c36cb8f145aa9a61428015cc47bac5ae38f1
apurva011104 <akaul7817@gmail.com>
2025-12-20 10:33:19 +0530

    [Apurva Gupta] Add .Added power of a number

---
+++ core-java-practice/gcr-codebase/control-flow/PowerOfANumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PowerOfANumber {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num=scan.nextInt();
+        System.out.print("Enter the power: ");
+        int power=scan.nextInt();
+        int result=1;
+        for(int i=1;i<=power;i++){
+            result*=num;
+        }
+        System.out.println(num+" raised to the power "+power+" is: "+result);
+        scan.close();
+    }
+}
