b2e034d5a7692d9069b87bbb0ee85af0f9159ef9
apurva011104 <akaul7817@gmail.com>
2025-12-23 12:35:42 +0530

    [Apurva Gupta] Refactor .Added stats visiblity after playing multiple rounds

--- core-java-practice/gcr-codebase/string/RockPaperScissor.java
+++ core-java-practice/gcr-codebase/string/RockPaperScissor.java
@@ -3,25 +3,35 @@ import java.util.Scanner;
 public class RockPaperScissor {
 
     final static String []rockPaperScissor = {"rock","paper","scissor"};
-
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        System.out.print("Enter rock/paper/scissor: ");
-        String userChoice=scan.next().toLowerCase();
-        if(!userChoice.equals("rock") && !userChoice.equals("paper") && !userChoice.equals("scissor") ){
-            System.out.println("Invalid choice!");
-            return;
+        int userWins=0, computerWins=0;
+        for(int i=0;i<10;i++){
+            System.out.print("Choose either rock/paper/scissor: ");
+            String userChoice=scan.next().toLowerCase();
+            while(!userChoice.equals("rock") && !userChoice.equals("paper") && !userChoice.equals("scissor")){
+                System.out.print("Invalid choice! Choose again: ");
+                userChoice=scan.next().toLowerCase();
+            }
+
+            String computerChoice= rockPaperScissor[(int)(Math.random()*2)];
+            if(computerChoice.equals(userChoice)){
+                System.out.println("Tie");
+            }
+            else if((computerChoice.equals("rock") && userChoice.equals("paper")) || (computerChoice.equals("paper") && userChoice.equals("scissor")) || (computerChoice.equals("scissor") && userChoice.equals("rock"))){
+                System.out.println("User wins");
+                userWins++;
+            }
+            else{
+                System.out.println("Computer wins");
+                computerWins++;
+            }
         }
-        
-        String computerChoice= rockPaperScissor[(int)(Math.random()*2)];
-        if(computerChoice.equals(userChoice)){
-            System.out.println("Tie");
-        }
-        else if((computerChoice.equals("rock") && userChoice.equals("paper")) || (computerChoice.equals("paper") && userChoice.equals("scissor")) || (computerChoice.equals("scissor") && userChoice.equals("rock"))){
-            System.out.println("User wins");
-        }
-        else{
-            System.out.println("Computer wins");
-        }
+        System.out.println("No.of rounds: 10");
+        System.out.println("User wins: "+userWins);
+        System.out.println("Computer wins: "+computerWins);
+        System.out.println("Tie matches: "+(10-userWins-computerWins));
+        System.out.println("User winning rate: "+(userWins*10)+"%");
+        System.out.println("Computer winning rate: "+(computerWins*10)+"%");
     }
 }
