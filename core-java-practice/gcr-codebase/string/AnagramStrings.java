8b70395c5280ee551d277d96ca8385d262fff394
apurva011104 <akaul7817@gmail.com>
2025-12-23 13:53:43 +0530

    [Apurva Gupta] Add . Added check if two strings are anagram

---
+++ core-java-practice/gcr-codebase/string/AnagramStrings.java
@@ -0,0 +1,32 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramStrings {
+
+    public static int[]characterFrequency(String text){
+        int []frequency=new int[256];
+        for(char ch: text.toCharArray()){
+            frequency[ch]++;
+        }
+        return frequency;
+    }
+
+    public static boolean isAnagram(String text1, String text2){
+        int []frequency1=characterFrequency(text1);
+        int []frequency2=characterFrequency(text2);
+        return Arrays.toString(frequency1).equals(Arrays.toString(frequency2));
+    }
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter first text: ");
+        String text1=scan.next();
+        System.out.print("Enter second text: ");
+        String text2=scan.next();
+
+        boolean isAnagram=isAnagram(text1, text2);
+
+        System.out.println("Are \""+text1+"\" and \""+text2+"\" anagram strings? "+isAnagram);
+    }
+}
