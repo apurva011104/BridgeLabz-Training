d4dee138df7656705b5802a928bb7159843edc53
apurva011104 <akaul7817@gmail.com>
2025-12-24 15:19:19 +0530

    [Apurva Gupta] Add .Added unit converter 2

---
+++ core-java-practice/gcr-codebase/methods/UnitConverter2.java
@@ -0,0 +1,21 @@
+public class UnitConverter2{
+    public static double convertInchesIntoMeters(double inches){
+        return inches*0.0254;
+    }
+
+    public static double convertMetersIntoInches(double meters){
+        return meters*39.3701;
+    }
+
+    public static double convertFeetIntoYards(double feets){
+        return feets/3;
+    }
+
+    public static double convertYardsIntoFeet(double yards){
+        return 3*yards;
+    }
+
+    public static double convertInchesIntoCentimeters(double inches){
+        return inches*2.54;
+    }
+}
