a963c2ef68d9bf3569f1b919ba7cbac75ceb5786
apurva011104 <akaul7817@gmail.com>
2025-12-29 16:28:24 +0530

    [Apurva Gupta] Add .Added start method to display welcome

---
+++ core-java-practice/scenario-based/LineComparison.java
@@ -0,0 +1,9 @@
+public class LineComparison {
+
+    public static void start(){
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+    public static void main(String[] args) {
+        start();
+    }
+}
