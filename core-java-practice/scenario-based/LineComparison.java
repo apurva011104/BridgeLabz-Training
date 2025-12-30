9ab1b6d7d9ba7aa0911d5bd438b2dff3b8381fa8
apurva011104 <akaul7817@gmail.com>
2025-12-30 10:50:57 +0530

    [Apurva Gupta] Refactor . Added UC2: comparing equality of length of two lines

--- core-java-practice/scenario-based/LineComparison.java
+++ core-java-practice/scenario-based/LineComparison.java
@@ -15,21 +15,44 @@ public class LineComparison {
         return new int[]{x,y};
     }
 
+    //Method to take line points coordinates
+    public static int[][] takeLinePointsInput(Scanner scan){
+        System.out.println("Enter point 1 coordinates: ");
+        int []point1 = takePointInput(scan);
+        System.out.println("Enter point 2 coordinates: ");
+        int []point2 = takePointInput(scan);
+        return new int[][]{point1,point2};
+    }
+
     //Method to calculate length of line
-    public static double calculateLength(int []point1 , int []point2){
-        double length = Math.sqrt( ( Math.pow(point2[0],2) - Math.pow(point1[0],2)) + ( Math.pow(point2[1],2) - Math.pow(point1[1],2) ));
+    public static Double calculateLength(int [][]linePoints){
+        int []point1 = linePoints[0];
+        int []point2 = linePoints[1];
+        Double length = Math.sqrt( ( Math.pow(point2[0] - point1[0],2)) + ( Math.pow(point2[1] - point1[1],2) ));
         return Math.round(length*100.0)/100.0;
+    }
+
+    //Method to check equality of length of the line
+    public static boolean checkLineEquality(Double length1 , Double length2){
+        return length1.equals(length2);
     }
 
     public static void main(String[] args) {
         start();
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter point 1 coordinates: ");
-        int []point1 = takePointInput(scan);
-        System.out.println("Enter point 2 coordinates: ");
-        int []point2 = takePointInput(scan);
-        double length = calculateLength(point1, point2);
-        System.out.printf("Length of the line: %.2f",length);
+        System.out.println("Enter line1 points coordinates: ");
+        int [][]line1 = takeLinePointsInput(scan);
+        System.out.println("Enter line2 points coordinates: ");
+        int [][]line2 = takeLinePointsInput(scan);
+
+        Double length1 = calculateLength(line1);
+        Double length2 = calculateLength(line2);
+
+        boolean isEqual = checkLineEquality(length1, length2);
+
+        System.out.printf("Length of the line1: %.2f %n",length1);
+        System.out.printf("Length of the line2: %.2f %n",length2);
+        System.out.println("Is line1 and line2 equal?: "+isEqual);
         scan.close();
     }
 }
