0d8eb77ddf4d4f9056100557677e853870abd703
apurva011104 <akaul7817@gmail.com>
2025-12-30 16:08:09 +0530

    [Apurva Gupta] Refactor. Added UC7: changed the code for 2 players and reporting the winner

--- core-java-practice/scenario-based/SnakeAndLadder.java
+++ core-java-practice/scenario-based/SnakeAndLadder.java
@@ -5,9 +5,11 @@ public class SnakeAndLadder {
 
         int playerNumber;
         int position;
+        int dieRolls;
         public Player(int playerNumber) {
             this.playerNumber= playerNumber;
             position=0;
+            dieRolls=0;
         }
 
         //Methods to roll the die
@@ -37,10 +39,9 @@ public class SnakeAndLadder {
     }
 
     //Initializing options variables
-    private static final int LADDER_OPTION = 1;
-    private static final int SNAKE_OPTION = -1;
-    private static final int NO_PLAY_OPTION = 0;
-
+    private static final String LADDER_OPTION = "Ladder";
+    private static final String SNAKE_OPTION = "Snake";
+    private static final String NO_PLAY_OPTION = "No play";
 
     //Method to check option
     private static int checkOption(){
@@ -52,38 +53,69 @@ public class SnakeAndLadder {
     public static String getOption(Player player, int dieNumber){
         int option = checkOption();
         switch(option){
-            case SNAKE_OPTION:
+            case -1:
                 player.updatePosition(-dieNumber);
-                return "Snake";
-            case LADDER_OPTION:
+                return SNAKE_OPTION;
+            case 1:
                 player.updatePosition(dieNumber);
-                return "Ladder";
+                return LADDER_OPTION;
             default:
-                return "No Play";
+                return NO_PLAY_OPTION;
         }
+    }
+
+    public static void printEachRoundData(Player player, int dieNumber, String playerOption){
+        System.out.println("Number player "+player.playerNumber +" got by rolling the die: "+dieNumber);
+        System.out.println("Player "+player.playerNumber +" option: "+playerOption);
+        System.out.println("Player "+player.playerNumber +" current position: "+player.position);
     }
 
     public static void main(String[] args) {
 
         System.out.println("Snake & Ladder Simulator");
 
+        //Creating 2 players
         Player player1 = new Player(1);
-        System.out.println("Player1's start position: "+player1.position);
-        int dieRolls=0;
+        Player player2 = new Player(2);
+        System.out.println("Player 1's start position: "+player1.position);
+        System.out.println("Player 2's start position: "+player2.position);
 
-        while(player1.position<100){
-            dieRolls++;
-            int dieNumber = player1.rollTheDie();
-            System.out.println("Number player1 got by rolling the die: "+dieNumber);
+        boolean isPlayer1Turn = true;       //Creating a boolean variable to keep track of player's turn
 
-            String player1Option = getOption(player1,dieNumber);
-            System.out.println("Player1 option: "+player1Option);
-            System.out.println("Player1's current position: "+player1.position);
+        while(player1.position<100 && player2.position<100){
+
+            Player player = isPlayer1Turn?player1:player2;
+            System.out.println("Player "+player.playerNumber+" turn");
+
+            player.dieRolls++;
+
+            //Die number player got after rolling the die
+            int dieNumber = player.rollTheDie();
+
+            //Getting option for the player
+            String playerOption = getOption(player,dieNumber);
+
+            //Reporting each round's data
+            printEachRoundData(player, dieNumber, playerOption);
             System.out.println("----------------------------------------------------------");
+
+            if(!playerOption.equals(LADDER_OPTION)){            //Changing turns if player did not get the ladder option
+                isPlayer1Turn=!isPlayer1Turn;
+            }
+
         }
-        System.out.println("Player1 reached 100");
-        System.out.println("Total die rolls: "+dieRolls);
 
+        //Reporting total die rolls
+        System.out.println("Player 1 total die rolls: "+player1.dieRolls);
+        System.out.println("Player 2 total die rolls: "+player2.dieRolls);
 
+        //Reporting winner
+        if(player1.position==100){
+            System.out.println("Player 1 won");
+        }
+        else{
+            System.out.println("Player 2 won");
+        }
+          
     }
 }
