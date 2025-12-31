d51749ff5fcbbb60736b8008210b6af845202fff
apurva011104 <akaul7817@gmail.com>
2025-12-31 23:00:13 +0530

    [Apurva Gupta] Add .Added Course (demonstrates class and instance variables and methods)

---
+++ oops-practice/gcr-codebase/constructors-variables-and-modifiers/Course.java
@@ -0,0 +1,69 @@
+public class Course {
+    
+    private String courseName;          //Instance variable
+    private int duration;           //Instance variable
+    private double fee;         //Instance variable
+    private static String instituteName = "GLA University";       //Class variable
+
+    //Getters and setters
+    public Course(String courseName , int duration, double fee) {
+        setCourseName(courseName);
+        setDuration(duration);
+        setFee(fee);
+    }
+
+    public void setCourseName(String courseName){
+        this.courseName = courseName;
+    }
+
+    public void setDuration(int duration){
+        this.duration = duration;
+    }
+
+    public void setFee(double fee){
+        this.fee = fee;
+    }
+
+    public static void setInstituteName(String newInstituteName){
+        instituteName = newInstituteName;
+    }
+
+    public String getCourseName(){
+        return courseName;
+    }
+
+    public int getDuration(){
+        return duration;
+    }
+
+    public double getFee(){
+        return fee;
+    }
+
+    //Class method to update institute name
+    public static void updateInstituteName(String newInstituteName){
+        instituteName = newInstituteName;
+    }
+
+    //Instance method to display course details
+    public void displayCourseDetails(){
+        System.out.println("----------------------------------------------------");
+        System.out.println("Institute name: "+instituteName);
+        System.out.println("Course Name: "+getCourseName());
+        System.out.println("Duration: "+getDuration()+" months");
+        System.out.printf("Fee: INR %.2f%n",getFee());
+        System.out.println("---------------------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        Course course1  = new Course("Cyber Security",4,10000.00);
+        course1.displayCourseDetails();
+        Course course2 = new Course("Software Development",6,14000.00);
+        course2.displayCourseDetails();
+        updateInstituteName("GLA");
+        course1.displayCourseDetails();
+        course2.displayCourseDetails();
+        Course course3 = new Course("Web Development", 5, 11000.00);
+        course3.displayCourseDetails();
+    }
+}
