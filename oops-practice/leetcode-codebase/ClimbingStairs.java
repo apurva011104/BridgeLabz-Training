0838a87adce2c0bd7f848fb96a1918348ef64579
apurva011104 <akaul7817@gmail.com>
2026-01-06 23:28:22 +0530

    [Apurva Gupta] Add. Added Climbing stairs

---
+++ oops-practice/leetcode-codebase/ClimbingStairs.java
@@ -0,0 +1,20 @@
+public class ClimbingStairs {
+    
+    public static int climbStairs(int n) {
+        int []dp=new int[n+1];
+        for(int i=0;i<=n;i++){
+            if(i<=2){
+                dp[i]=i;
+            }
+            else{
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println("Number of ways to climb "+n+" stairs: "+climbStairs(n));
+    }
+}
