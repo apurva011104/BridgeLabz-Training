a82c9af09477fd0a5fbd734d34a111deb5331f47
apurva011104 <akaul7817@gmail.com>
2026-01-07 15:18:30 +0530

    [Apurva Gupta] Add .Added subclass User which extends Person

---
+++ oops-practice/scenario-based/ride-management-system/User.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class User extends Person {
+
+    private static  Set<String> allUserID = new HashSet<>();
+
+    public User(String name, String phoneNumber) throws InvalidPhoneNumberException, PhoneNumberAlreadyExistsException{  
+        super(generateID(),name,phoneNumber);
+    }
+
+    private static boolean isUniqueID(String id){
+        return !allUserID.contains(id);
+    }
+
+    private static String generateID(){
+        String userId ="USR";
+        do {
+            StringBuilder id = new StringBuilder();
+            for (int i = 0; i < 9; i++) {
+                id.append((char) ('0' + (int)(Math.random() * 10)));
+            }
+            userId += id.toString();
+        } 
+        while (!isUniqueID(userId));
+
+        allUserID.add(userId);
+        return userId;
+    }
+
+
+}
