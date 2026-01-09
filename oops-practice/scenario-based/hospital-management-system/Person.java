bcf1ab750937dbb869f043ad99e50d5158aaba7d
apurva011104 <akaul7817@gmail.com>
2026-01-09 14:42:17 +0530

    [Apurva Gupta] Add . Added Person superclass

---
+++ oops-practice/scenario-based/hospital-management-system/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    
+    private final String ID;
+    private String name;
+
+    public Person(String ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+}
