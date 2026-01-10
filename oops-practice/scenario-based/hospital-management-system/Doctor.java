21417ad352b35fd998abfee47613e5f3156e57a9
apurva011104 <akaul7817@gmail.com>
2026-01-11 00:02:04 +0530

    [Apurva Gupta] Refactor. Added availability field to check if doctor is available or not

--- oops-practice/scenario-based/hospital-management-system/Doctor.java
+++ oops-practice/scenario-based/hospital-management-system/Doctor.java
@@ -1,11 +1,14 @@
 public class Doctor extends Person{
 
     private String specialization;
+    private boolean availability;
 
     public Doctor(String ID, String name, String specialization) {
         super(ID, name);
         this.specialization = specialization;
+        this.availability = true;
     }
+    
 
     @Override
     public void displayInfo(){
@@ -13,5 +16,13 @@ public class Doctor extends Person{
         System.out.println("ID: "+getID());
         System.out.println("Name: "+getName());
         System.out.println("Specialization: "+specialization);
+    }
+
+    public boolean getAvailability() {
+        return availability;
+    }
+
+    public void updateAvailability(boolean availability) {
+        this.isAvailable = availability;
     }
 }
