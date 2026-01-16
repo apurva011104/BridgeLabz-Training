1cfa1e3de9e136338fb6401c1afff4f2a9ec9b57
apurva011104 <akaul7817@gmail.com>
2026-01-16 16:10:19 +0530

    [Apurva Gupta] UC2: Refactor. Moved the displaying information of a contact method to address book main class

--- address-book/Main.java
+++ address-book/Main.java
@@ -1,25 +1,17 @@
 public class Main{
 
-    public static void displayContactInformation(Contacts contact){
-        System.out.printf("Contact name: %s %s%n",contact.getFirstName(),contact.getLastName());
-        System.out.println("Address: "+contact.getAddress());
-        System.out.println("City: "+contact.getCity());
-        System.out.println("State: "+contact.getState());
-        System.out.println("ZIP Code: "+contact.getZip());
-        System.out.println("Phone Number: "+contact.getPhoneNumber());
-        System.out.println("Email ID: "+contact.getEmail());
-    }
-
-
     public static void main(String[] args){
         System.out.println("Start");
+        AddressBookMain addressBook = new AddressBookMain();
+
+        Contacts contact = addressBook.addContact();
         try {
-            Contacts contact1 = new Contacts("Apurva","Gupta","XYZ lane","Agra","UP","282001","8475284522","apurva@google.com");
-            displayContactInformation(contact1);
+            addressBook.displayContactInformation(contact);
         } 
         catch (Exception e) {
             System.out.println(e);
         }
+        
 
     }
 }
