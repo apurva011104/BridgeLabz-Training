6842af23d32a7dbd2d8fc843e924ff607d325445
apurva011104 <akaul7817@gmail.com>
2026-01-19 23:52:27 +0530

    [Apurva Gupta] Refactor . Added statements to check UC3

--- oops-practice/gcr-codebase/address-book/Main.java
+++ oops-practice/gcr-codebase/address-book/Main.java
@@ -1,23 +1,14 @@
 public class Main {
 
-    public static void displayInfo(Contact contact){
-        System.out.println("Contact details: ");
-        System.out.println("Name: "+contact.getFirstName()+" "+contact.getLastName());
-        System.out.println("Address: "+contact.getAddress());
-        System.out.println("City: "+contact.getCity());
-        System.out.println("ZIP Code: "+contact.getZip());
-        System.out.println("Phone Number: "+contact.getPhoneNumber());
-        System.out.println("Email ID: "+contact.getEmail());
-    }
-
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program.");
 
+        AddressBookMain addressBookMain = new AddressBookMain();
+        addressBookMain.addContact();
         try {
-            Contact contact = new Contact("Apurva","Gupta","XYZ Lane","XYZ city","XYZ state","123456","1234567890","apurva@google.com");
-            displayInfo(contact);
+            addressBookMain.updateContact();
         } 
-        catch (Exception e) {
+        catch (InvalidContactException e) {
             System.out.println(e);
         }
         
