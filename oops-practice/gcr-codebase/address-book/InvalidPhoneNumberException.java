394c790d035098bd289cec1466ee9235c46f6616
apurva011104 <akaul7817@gmail.com>
2026-01-19 23:44:52 +0530

    [Apurva Gupta] Delete. Deleted as replaced by another exception class

--- oops-practice/gcr-codebase/address-book/InvalidPhoneNumberException.java
+++
@@ -1,7 +0,0 @@
-public class InvalidPhoneNumberException extends Exception{
-
-    public InvalidPhoneNumberException(String message) {
-        super(message);
-    }
-    
-}
--- oops-practice/gcr-codebase/address-book/InvalidZIPException.java
+++
@@ -1,7 +0,0 @@
-public class InvalidZIPException extends Exception{
-
-    public InvalidZIPException(String message) {
-        super(message);
-    }
-    
-}
