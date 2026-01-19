56101e2de581bd01d4e2d05ff20c77055fcd72e2
apurva011104 <akaul7817@gmail.com>
2026-01-19 12:09:27 +0530

    [Apurva Gupta] Refactor . Added statements to test Contact class

--- oops-practice/gcr-codebase/address-book/Main.java
+++ oops-practice/gcr-codebase/address-book/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+
+    public static void displayInfo(Contact contact){
+        System.out.println("Contact details: ");
+        System.out.println("Name: "+contact.getFirstName()+" "+contact.getLastName());
+        System.out.println("Address: "+contact.getAddress());
+        System.out.println("City: "+contact.getCity());
+        System.out.println("ZIP Code: "+contact.getZip());
+        System.out.println("Phone Number: "+contact.getPhoneNumber());
+        System.out.println("Email ID: "+contact.getEmail());
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program.");
+
+        try {
+            Contact contact = new Contact("Apurva","Gupta","XYZ Lane","XYZ city","XYZ state","123456","1234567890","apurva@google.com");
+            displayInfo(contact);
+        } 
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        
     }
 }
