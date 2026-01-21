0f42741a1383902b64f4fd677dfad97e6c40b486
apurva011104 <akaul7817@gmail.com>
2026-01-21 22:59:52 +0530

    [Apurva Gupta] Add. Added class Product Manager abstract class Job Role

---
+++ collections-practice/gcr-codebase/java-generics/ai-driven-resume-screening-system/ProductManager.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class ProductManager  extends JobRole {
+
+    public ProductManager() {
+        super(Arrays.asList("Excel","Communication","Data Analysis","Leadership","Agile framework"),
+        "Product Manager",24);
+    }
+    
+}
