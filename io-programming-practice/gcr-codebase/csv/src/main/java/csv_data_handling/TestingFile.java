125587557fd518c85ba113a5b742e5118d349436
apurva011104 <akaul7817@gmail.com>
2026-02-02 11:36:22 +0530

    [Apurva Gupta] Add. Added class StudentFileReader and it's respective testing class

---
+++ io-programming-practice/gcr-codebase/csv/src/main/java/csv_data_handling/TestingFile.java
@@ -0,0 +1,7 @@
+package csv_data_handling;
+
+public class TestingFile{
+    public static boolean testing(int x){
+        return x==5;
+    }
+}
---
+++ io-programming-practice/gcr-codebase/csv/src/test/java/csv_data_handling/TestingFileTest.java
@@ -0,0 +1,13 @@
+package csv_data_handling;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestingFileTest {
+    
+    @Test
+    public void Test(){
+        assertTrue(TestingFile.testing(5));
+    }
+}
